5c8acb4d2b9e5492f5b3c67be166ff7cfc67bc25
Osakpolor Omonbude <omonbude.emma@gmail.com>
2026-09-23 22:07:34 +0100

    barchart, grouped bar chart, histogram, boxplot and scattered plot added to my-dashboard

--- my-dashboard/charts.py
+++ my-dashboard/charts.py
@@ -1,9 +1,9 @@
 import plotly.express as px
 stocks = px.data.stocks()   # built-in dataset: ships with plotly
-gapminder = px.data.gapminder()
-pop = gapminder.groupby(['year','continent'], as_index=False)['pop'].sum()
+
 
 # line chart
+gapminder = px.data.gapminder()
 fig = px.line(
     stocks, x='date', y='GOOG',
     title='Google Stock Price (Normalised)',
@@ -13,10 +13,71 @@ fig.update_traces(line=dict(width=2.5, color='#00B4D8'))
 fig.show()
 
 # area chart
+pop = gapminder.groupby(['year','continent'], as_index=False)['pop'].sum()
 fig = px.area(
     pop, x='year', y='pop', color='continent',
     # groupnorm='fraction',   # bands stack to 100% to show each continent's share
     title='World Population by Continent',
     template='plotly_white'
+)
+fig.show()
+
+# barchart
+tips = px.data.tips()
+daily = tips.groupby('day', as_index=False)['total_bill'].sum() 
+fig = px.bar(
+    daily, x='day', y='total_bill',
+    color='day', text_auto=True,
+    category_orders={'day': ['Thur','Fri','Sat','Sun']},  # weekdays in order
+    title='Total Restaurant Bills by Day',
+    template='plotly_white'
+)
+fig.update_layout(showlegend=False)
+fig.show()
+
+# grouped bar chart
+
+avg = tips.groupby(['day','sex'], as_index=False)['total_bill'].mean()
+fig = px.bar(
+    avg, x='day', y='total_bill',
+    color='sex', barmode='group',   # try barmode='stack'
+    category_orders={'day': ['Thur','Fri','Sat','Sun']},
+    title='Average Bill by Day and Sex',
+    template='plotly_white',
+    text_auto=True,
+)
+fig.show()
+
+# histogram
+
+fig = px.histogram(
+    tips, x='total_bill', nbins=20,
+    title='Distribution of Total Bills',
+    template='plotly_white', text_auto=True
+)
+fig.update_traces(
+    marker_color='brown',
+    marker_line_width=1, marker_line_color='white')
+fig.show()
+
+# box plot
+
+fig = px.box(
+    tips, x='day', y='total_bill', color='smoker',
+    # points='all',   # draw every observation as dots beside each box
+    category_orders={'day': ['Thur','Fri','Sat','Sun']},
+    title='Bill Distribution by Day',
+    template='plotly_white'
+)
+fig.show()
+
+# scattered plot
+# trendline needs statsmodels: pip install statsmodels
+fig = px.scatter(
+    tips, x='total_bill', y='tip',
+    color='smoker', size='size',
+    hover_name='day',
+    trendline='lowess',   # options: 'ols', 'lowess', 'rolling', 'ewm', 'expanding'
+    title='Tips vs Total Bill', template='plotly_white'
 )
 fig.show()
