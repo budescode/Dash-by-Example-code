5b36b9df096dc47654cfd2496326f1462018405e
Osakpolor Omonbude <omonbude.emma@gmail.com>
2026-09-24 17:31:53 +0100

    state.py added to my-dashboard for chapter 4.8

---
+++ my-dashboard/state.py
@@ -0,0 +1,35 @@
+import dash
+from dash import html, Input, Output, State, callback
+import dash_bootstrap_components as dbc
+import pandas as pd
+ 
+df = pd.DataFrame({
+    'Product Name': ['Blue Chair', 'Red Desk', 'Blue Lamp', 'Green Sofa'],
+    'Sales':        [120, 340, 90, 560],
+})
+ 
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+ 
+app.layout = dbc.Container([
+    dbc.Input(id='search-input',
+              placeholder='Search products, e.g. red, blue, green',
+              type='text', className='mt-3'),
+    dbc.Button('Search', id='search-btn', color='primary',
+               className='mt-2'),
+    html.Pre(id='results', className='mt-3'),
+])
+ 
+@callback(
+    Output('results', 'children'),
+    Input('search-btn', 'n_clicks'),
+    State('search-input', 'value'),
+    prevent_initial_call=True
+)
+def search(n_clicks, query):
+    if not query:
+        return 'Type something, then press Search.'
+    hits = df[df['Product Name'].str.contains(query, case=False)]
+    return hits.to_string(index=False)
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
